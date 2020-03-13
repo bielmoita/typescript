649c355e8a44caa9c7ad96dc4e8e39b4660407fa
Gabriel Fernando <oficial.gabriel89@gmail.com>
2020-03-13 12:24:16 -0300

    TypeScript Aprendendo

---
+++ EX2/funcao.js
@@ -0,0 +1,11 @@
+function concatena(nome, sobrenome) {
+    // console.log(`Bom dia ${nome} ${sobrenome}`)
+    return "Bom dia " + nome + " " + sobrenome;
+}
+//Invocando a função
+concatena("Maria", "Top");
+// concatena(true),
+// concatena(2),
+// concatena("2");
+//Invocando a função outra maneira usando o return
+console.log(concatena("Maria", "Top"));
---
+++ EX2/funcao.ts
@@ -0,0 +1,14 @@
+function concatena(nome:string, sobrenome:string)
+{
+    // console.log(`Bom dia ${nome} ${sobrenome}`)
+    return `Bom dia ${nome} ${sobrenome}`
+}
+
+//Invocando a função
+concatena("Maria", "Top")
+// concatena(true),
+// concatena(2),
+// concatena("2");
+
+//Invocando a função outra maneira usando o return
+console.log(concatena("Maria", "Top"))
